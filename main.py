697e63d94b5f3559f16aeb2263e8a8139ccd3a2e
Jorrit de Boer <jorritdb@hotmail.com>
2024-03-03 22:22:48 +0100

    Add UCB-C (pick random on tie)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import plot
 import policies
@@ -17,43 +18,67 @@ class MAB:
         )
 
 
+# %%
 v1 = 0.5
 v2 = 0.2
 
-bandit_probs = [v1, v1 - v2]
+bandit_probs = [v1, v1 + v2, v2, 0.7 * v1 + v2]
+# bandit_probs = [v1, v1 + v2, v2, 0.7 * v1 + v2]
 mab = MAB(bandit_probs)
+
+v1 = cp.Variable()
+v2 = cp.Variable()
+
+p1max = cp.Parameter()
+p1min = cp.Parameter()
+p2max = cp.Parameter()
+p2min = cp.Parameter()
+p3max = cp.Parameter()
+p3min = cp.Parameter()
+p4max = cp.Parameter()
+p4min = cp.Parameter()
+
+eq1 = v1
+eq2 = v1 + v2
+eq3 = v2
+eq4 = 0.7 * v1 + v2
+
+constraints = [
+    v1 >= 0,
+    v1 <= 1,
+    v2 >= 0,
+    v2 <= 1,
+    eq1 >= p1min,
+    eq1 <= p1max,
+    eq2 >= p2min,
+    eq2 <= p2max,
+    eq3 >= p3min,
+    eq3 <= p3max,
+    eq4 >= p4min,
+    eq4 <= p4max,
+]
+
+prob1 = cp.Problem(cp.Maximize(eq1), constraints)
+prob2 = cp.Problem(cp.Maximize(eq2), constraints)
+prob3 = cp.Problem(cp.Maximize(eq3), constraints)
+prob4 = cp.Problem(cp.Maximize(eq4), constraints)
 
 
 def dep_max_est(estimators, bound):
-    v1 = cp.Variable()
-    v2 = cp.Variable()
+    p1min.value = estimators[0] - bound[0]
+    p1max.value = estimators[0] + bound[0]
+    p2min.value = estimators[1] - bound[1]
+    p2max.value = estimators[1] + bound[1]
+    p3min.value = estimators[2] - bound[2]
+    p3max.value = estimators[2] + bound[2]
+    p4min.value = estimators[3] - bound[3]
+    p4max.value = estimators[3] + bound[3]
 
-    eq1 = v1
-    eq2 = v1 - v2
-
-    constraints = [
-        v1 >= 0,
-        v1 <= 1,
-        v2 >= 0,
-        v2 <= 1,
-        eq1 >= estimators[0] - bound[0],
-        eq1 <= estimators[0] + bound[0],
-        eq2 <= estimators[1] + bound[1],
-        eq2 >= estimators[1] - bound[1],
-        # v1 + v2 <= estimators[2] + bound[2],
-        # v1 + v2 >= estimators[2] - bound[2],
-    ]
-
-    obj = cp.Maximize(eq1)
-    prob = cp.Problem(obj, constraints)
-    p1_max_est = prob.solve()
-    obj = cp.Maximize(eq2)
-    prob = cp.Problem(obj, constraints)
-    p2_max_est = prob.solve()
-    # obj = cp.Maximize(v1 + v2)
-    # prob = cp.Problem(obj, constraints)
-    # p3_max_est = prob.solve()
-    return p1_max_est, p2_max_est
+    p1_max_est = prob1.solve(solver=cp.SCS)
+    p2_max_est = prob2.solve(solver=cp.SCS)
+    p3_max_est = prob3.solve(solver=cp.SCS)
+    p4_max_est = prob4.solve(solver=cp.SCS)
+    return p1_max_est, p2_max_est, p3_max_est, p4_max_est
 
 
 # dependent UCB
@@ -72,6 +97,7 @@ def choose_bandit(k_array, reward_array, n_bandits):
 
     dep_bounds = dep_max_est(success_ratio, sqrt_term)
 
+    # return np.argmax(success_ratio + sqrt_term)
     return np.argmax(
         [min((success_ratio + sqrt_term)[i], dep_bounds[i]) for i in range(n_bandits)]
     )
@@ -81,14 +107,23 @@ def random_policy(k_array, reward_array, n_bandits):
     return np.random.choice(range(n_bandits), 1)[0]
 
 
-plot.plot_MAB_experiment(choose_bandit, mab, 100, bandit_probs, "Bounded UCB")
+# %%
+plot.plot_MAB_experiment(
+    choose_bandit, mab, 1000, bandit_probs, "Dependent UCB", graph=False, video=False
+)
 
 
+# %%
 algorithms = {
+    # "random": random_policy,
     "e_greedy": policies.eGreedyPolicy(0.1).choose_bandit,
     "ucb": policies.UCBPolicy().choose_bandit,
     "ts": policies.TSPolicy().choose_bandit,
-    "ucb-bounded": policies.TSPolicy().choose_bandit,
+    "ucb-B": policies.UCBPolicyB().choose_bandit,
+    "ucb-C": policies.UCBPolicyC().choose_bandit,
+    "ucb-dependent": choose_bandit,
 }
 
-simulation(mab, algorithms, 100, 100)
+simulation(mab, algorithms, 1000, 5)
+
+# %%
--- policies.py
+++ policies.py
@@ -88,6 +88,36 @@ class UCBPolicyB:
         )
 
 
+# upper confidence bound policy
+# version C, where we max out the upper confidence at 1
+# but, in case multiple upper bounds are maximal, we pick random from those
+class UCBPolicyC:
+
+    # initializing
+    def __init__(self):
+
+        # nothing to do here
+        pass
+
+    # choice of bandit
+    def choose_bandit(self, k_array, reward_array, n_bandits):
+        # sucesses and total draws
+        success_count = reward_array.sum(axis=1)
+        total_count = k_array.sum(axis=1)
+
+        # ratio of sucesses vs total
+        success_ratio = success_count / total_count
+
+        # computing square root term
+        sqrt_term = np.sqrt(2 * np.log(np.sum(total_count)) / total_count)
+
+        upper_bounds = [
+            min(1, success_ratio[i] + sqrt_term[i]) for i in range(n_bandits)
+        ]
+        # returning best greedy action
+        return np.random.choice(np.flatnonzero(upper_bounds == np.max(upper_bounds)))
+
+
 # e-greedy policy
 class TSPolicy:
 
