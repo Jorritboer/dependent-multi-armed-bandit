359c2efbda92feaca3e25596e91eca9d8a394ab4
Jorrit de Boer <jorritdb@hotmail.com>
2024-04-02 21:25:44 +0200

    Add ucb tuned dependent

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MAB:
 v1 = 0.5
 v2 = 0.2
 
-bandit_probs = [v1, v1 + v2]
+bandit_probs = [v1, v1 + v2, v2, 0.7 * v1 + v2]
 
 mab = MAB(bandit_probs)
 
@@ -30,7 +30,7 @@ v1 = cp.Variable()
 v2 = cp.Variable()
 # v3 = cp.Variable()
 variables = [v1, v2]
-bandit_expressions = [v1, v1 + v2]
+bandit_expressions = [v1, v1 + v2, v2, 0.7 * v1 + v2]
 
 
 # %%
@@ -47,12 +47,15 @@ plot.plot_MAB_experiment(
 # %%
 algorithms = {
     # "random": policies.RandomPolicy().choose_bandit,
-    "e_greedy": policies.eGreedyPolicy(0.1).choose_bandit,
+    # "e_greedy": policies.eGreedyPolicy(0.1).choose_bandit,
     # "ucb": policies.UCBPolicy().choose_bandit,
     "ts": policies.TSPolicy().choose_bandit,
     # "ucb-B": policies.UCBPolicyB().choose_bandit,
-    "ucb-C": policies.UCBPolicyC().choose_bandit,
+    # "ucb-C": policies.UCBPolicyC().choose_bandit,
     "ucb-tuned": policies.UCBPolicyTuned().choose_bandit,
+    "ucb-tuned_dep": policies.UCBPolicyTuned_Dependent(
+        variables, bandit_expressions
+    ).choose_bandit,
     # "ucb-dependent": policies.UCB_Dependent(
     #     variables, bandit_expressions
     # ).choose_bandit,
