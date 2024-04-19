ba3b1b5974244731f26b5e33acfd7854af1580a1
Jorrit de Boer <jorritdb@hotmail.com>
2024-04-19 16:53:08 +0200

    Add decaying epsilon

--- main.py
+++ main.py
@@ -19,18 +19,17 @@ class MAB:
 
 
 # %%
-v1 = 0.4
-v2 = 0.3
+v1 = 0.5
+v2 = 0.2
 
-bandit_probs = [1.2 * v1, v1 + v2 - 0.15, v2 + 0.10, 0.6 * v1 + 0.7 * v2 + 0.20]
-
+bandit_probs = [v1, v1 + v2, v2, 0.7 * v1 + v2]
 mab = MAB(bandit_probs)
 
 v1 = cp.Variable()
 v2 = cp.Variable()
 # v3 = cp.Variable()
 variables = [v1, v2]
-bandit_expressions = [1.2 * v1, v1 + v2 - 0.15, v2 + 0.10, 0.6 * v1 + 0.7 * v2 + 0.20]
+bandit_expressions = [v1, v1 + v2, v2, 0.7 * v1 + v2]
 
 
 # %%
@@ -44,10 +43,18 @@ plot.plot_MAB_experiment(
     graph=True,
 )
 
+
 # %%
+def lin_decaying(start, end, steps):
+    return lambda n: start - (n / steps) * (start - end) if n < steps else end
+
+
 algorithms = {
     # "random": policies.RandomPolicy().choose_bandit,
-    # "e_greedy": policies.eGreedyPolicy(0.1).choose_bandit,
+    "e_greedy": policies.eGreedyPolicy(0.1).choose_bandit,
+    "e_greedy_decaying": policies.eGreedyPolicyDecaying(
+        lin_decaying(0.75, 0.005, 500)
+    ).choose_bandit,
     # "ucb": policies.UCBPolicy().choose_bandit,
     "ts": policies.TSPolicy().choose_bandit,
     # "ucb-B": policies.UCBPolicyB().choose_bandit,
@@ -58,16 +65,16 @@ algorithms = {
     # "ucb-dependentB": policies.UCB_DependentB(
     #     variables, bandit_expressions
     # ).choose_bandit,
-    "ucb2(0.5)": policies.UCBPolicy2(0.5, len(bandit_probs)).choose_bandit,
+    # "ucb2(0.5)": policies.UCBPolicy2(0.5, len(bandit_probs)).choose_bandit,
     # "ucb2_dep(0.5)": policies.UCBPolicy2_dependent(
     #     0.5, variables, bandit_expressions
     # ).choose_bandit,
-    # "ucb-tuned": policies.UCBPolicyTuned().choose_bandit,
+    "ucb-tuned": policies.UCBPolicyTuned().choose_bandit,
     # "ucb-tuned_dep": policies.UCBPolicyTuned_Dependent(
     #     variables, bandit_expressions
     # ).choose_bandit,
 }
 
-simulation(mab, algorithms, 10000, 10, plot_reward=False, plot_uncertainty=True)
+simulation(mab, algorithms, 2000, 10, plot_reward=False, plot_uncertainty=True)
 
 # %%
--- policies.py
+++ policies.py
@@ -49,6 +49,33 @@ class eGreedyPolicy:
             return np.argmax(success_ratio)
 
 
+# e-greedy policy with decaying epsilon
+class eGreedyPolicyDecaying:
+    # epsilon is now a function
+    def __init__(self, epsilon_func):
+        self.epsilon_func = epsilon_func
+
+    def choose_bandit(self, k_array, reward_array, n_bandits):
+
+        # sucesses and total draws
+        success_count = reward_array.sum(axis=1)
+        total_count = k_array.sum(axis=1)
+
+        # try out each arm once, and avoid dividing by 0 below
+        for k in range(n_bandits):
+            if total_count[k] == 0:
+                return k
+
+        success_ratio = success_count / total_count
+
+        epsilon = self.epsilon_func(np.sum(total_count))
+        if np.random.random() < epsilon:
+            return np.random.choice(
+                np.delete(list(range(n_bandits)), np.argmax(success_ratio))
+            )
+        return np.argmax(success_ratio)
+
+
 # upper confidence bound policy
 class UCBPolicy:
 
